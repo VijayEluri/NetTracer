efc9782bca46e4b6a52962c97c36c61a7419c39f
Peter Hofmann <scm@uninformativ.de>
2010-03-01 15:49:03 +0100

    [core] Replaced SConstruct with ant.

--- run-headless.sh
+++ run-headless.sh
@@ -11,22 +11,15 @@ fi
 TARGET="$1"
 shift
 
-if [[ -z $1 ]]
-then
-	RENDERFILE="../scenes/example4.scn"
-else
-	RENDERFILE=../"$1"
-fi
-
-cd build
 if [[ $NICE == "YES" ]]
 then
 	nice -n 19 java -Xms1884m -Xmx1885m \
 		-XX:+UseConcMarkSweepGC -XX:+UseParNewGC \
-		RaytracerApplication -h "$TARGET" "$RENDERFILE"
+		-cp antDist/Raytracer.jar \
+		RaytracerApplication -h "$TARGET" "$@"
 else
 	java -Xms1884m -Xmx1885m \
 		-XX:+UseConcMarkSweepGC -XX:+UseParNewGC \
-		RaytracerApplication -h "$TARGET" "$RENDERFILE"
+		-cp antDist/Raytracer.jar \
+		RaytracerApplication -h "$TARGET" "$@"
 fi
-cd -
--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-if [[ -z $1 ]]
-then
-	RENDERFILE="../scenes/example4.scn"
-else
-	RENDERFILE=../"$1"
-fi
-
-cd build
-java -Xms1884m -Xmx1885m RaytracerApplication "$RENDERFILE"
-cd -
+java -Xms1884m -Xmx1885m -cp antDist/Raytracer.jar \
+	RaytracerApplication "$@"
